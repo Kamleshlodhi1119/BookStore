f057562b54cb8f0bf4587f52b2657b1ffac62a0f
Kamleshlodhi1119 <kamleshlodhi9302#gmail.com>
2026-01-10 12:11:47 +0530

    add login

--- bookstore_project/src/main/java/com/bookstore/controller/AuthController.java
+++ bookstore_project/src/main/java/com/bookstore/controller/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://books-storeapp.netlify.app/")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
