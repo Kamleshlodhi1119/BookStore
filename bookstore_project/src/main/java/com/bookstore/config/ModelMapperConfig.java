e697c12bf643d29912f25c2678e95428bc31f6ce
Kamleshlodhi1119 <kamleshlodhi9302#gmail.com>
2026-01-15 21:13:06 +0530

    mapper added

--- bookstore_project/src/main/java/com/bookstore/config/ModelMapperConfig.java
+++ bookstore_project/src/main/java/com/bookstore/config/ModelMapperConfig.java
@@ -4,10 +4,30 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.bookstore.dto.BookDto;
+import com.bookstore.entity.Book;
+
 @Configuration
 public class ModelMapperConfig {
+//	@Bean
+//	public ModelMapper modelMapper() {
+//		return new ModelMapper();
+//	}
+	
+	
 	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+
+        mapper.typeMap(Book.class, BookDto.class)
+              .addMappings(m -> {
+                  m.map(
+                      src -> src.getAuthor() != null ? src.getAuthor().getName() : null,
+                      BookDto::setAuthorName
+                  );
+                  m.map(Book::getAverageRating, BookDto::setAverageRating);
+              });
+
+        return mapper;
 	}
 }
